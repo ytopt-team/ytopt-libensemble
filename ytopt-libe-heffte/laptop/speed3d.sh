79592add46648320e40951cf3fa9f72e807b3192
Xingfu Wu <wuxf99@gmail.com>
2023-03-30 15:59:50 -0500

    modified:   ytopt-libe-heffte/crusher/speed3d.sh 	modified:   ytopt-libe-heffte/laptop/speed3d.sh 	modified:   ytopt-libe-heffte/theta/speed3d.sh

--- ytopt-libe-heffte/laptop/speed3d.sh
+++ ytopt-libe-heffte/laptop/speed3d.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 
-/Users/xingfu/research/Y-TUNE/Crusher/heffte/build/benchmarks/speed3d_r2c fftw #P0 #P1 #P1 #P1 #P2 #P3 #P4 #P5 #P6 #P7 #P8 
+speed3d_r2c fftw #P0 #P1 #P1 #P1 #P2 #P3 #P4 #P5 #P6 #P7 #P8 
